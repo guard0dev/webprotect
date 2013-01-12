5e794fb12320c6f74ecf1b9fd207f10ecaf0750f
Jan <killerpenguin20@googlemail.com>
2013-01-12 19:16:07 +0100

    Files

---
+++ webguard/admin/index.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+<link rel="stylesheet" type="text/css" href="/include/formate.css">
+    </head>
+    <body>
+        
+    </body>
+</html>
